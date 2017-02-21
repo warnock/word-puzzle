525d3cca8a21bf0b6432f50bd561dd54fff1ec09
Chris Wainner and Megan Warnock <megan@>
2017-02-21 09:15:51 -0800

    Add second box to input guess, and add output notification

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,30 @@
-var originalString = "Believe you can and you're halfway there. Theodore Roosevelt";
-var parString = originalString.slice();
-var splitString = parString.split("");
+var originalString = "";
 
 $(function(){
-  for(var index = 0; index < splitString.length; index++){
-    if(splitString[index] === "a" || splitString[index] === "e" || splitString[index] === "i" || splitString[index] === "o" || splitString[index] === "u"){
-      splitString[index] = "-";
+  $("#formOne").submit(function(){
+    event.preventDefault();
+    originalString = $("#input1").val();
+    var parString = originalString;
+    var splitString = parString.split("");
+
+    for(var index = 0; index < splitString.length; index++){
+      if(splitString[index] === "a" || splitString[index] === "e" || splitString[index] === "i" || splitString[index] === "o" || splitString[index] === "u"){
+        splitString[index] = "-";
+      }
     }
-  }
-  parString = splitString.join("");
-  $("#stringBox").text(parString);
+    parString = splitString.join("");
+
+    $("#formOne").hide();
+    $("#outputBox").show();
+    $("#stringBox").text(parString);
+  });
+  $("#formTwo").submit(function(){
+    event.preventDefault();
+    var guessString = $("#input2").val();
+    if(guessString === originalString){
+      $("#outputInfo").text("Yay! You got it!");
+    } else{
+      $("#outputInfo").text("Not quite, please try again.")
+    }
+  });
 });
